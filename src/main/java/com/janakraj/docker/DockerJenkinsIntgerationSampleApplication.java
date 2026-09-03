1dfd005349dd5314e3a15a435980b29270df3650
Janakraj Khatal <janakrajkhatal@gmail.com>
2026-09-03 09:30:51 +0530

    first commit

---
+++ src/main/java/com/janakraj/docker/DockerJenkinsIntgerationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.janakraj.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntgerationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntgerationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janakraj/docker/DockerJenkinsIntgerationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janakraj.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntgerationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
