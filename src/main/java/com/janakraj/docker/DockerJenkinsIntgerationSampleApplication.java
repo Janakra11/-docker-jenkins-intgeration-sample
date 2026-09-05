7d10de3ac39cb0efb59304444bb91467114df951
Janakra11 <janakrajkhatal@gmail.com>
2026-09-05 22:18:24 +0530

    Added some changes

--- src/main/java/com/janakraj/docker/DockerJenkinsIntgerationSampleApplication.java
+++ src/main/java/com/janakraj/docker/DockerJenkinsIntgerationSampleApplication.java
@@ -1,12 +1,23 @@
 package com.janakraj.docker;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntgerationSampleApplication {
 
+	private static final Logger logger = LogManager.getLogger(DockerJenkinsIntgerationSampleApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application started");
+	}
+
 	public static void main(String[] args) {
+		logger.info("Application executed");
 		SpringApplication.run(DockerJenkinsIntgerationSampleApplication.class, args);
 	}
 
--- src/test/java/com/janakraj/docker/DockerJenkinsIntgerationSampleApplicationTests.java
+++ src/test/java/com/janakraj/docker/DockerJenkinsIntgerationSampleApplicationTests.java
@@ -1,13 +1,21 @@
 package com.janakraj.docker;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerJenkinsIntgerationSampleApplicationTests {
 
+	private static final Logger logger = LogManager.getLogger(DockerJenkinsIntgerationSampleApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("Test executed...");
+		logger.info("Test executing second log statements...");
+		Assertions.assertEquals(true,true);
 	}
 
 }
